e706a9823136a1b2791a6ef83f2c6484dcb728fc
vivekdevaraj96 <121920875+vivekdevaraj96@users.noreply.github.com>
2023-01-17 09:05:35 -0800

    Add files via upload

---
+++ JSONFor.js
@@ -0,0 +1,27 @@
+var stu=[
+    {
+        "name":"Vivek",
+        "age" :"25"
+    },{
+        "name":"Vignesh",
+        "age" :"24"
+    },{
+        "name":"Dinesh",
+        "age" :"25"
+    },{
+        "name":"Rasanth",
+        "age" :"24"
+    }
+]
+
+for(i=0;i<stu.length;i++){
+    console.log(`Age of ${stu[i].name} is ${stu[i].age}`);
+}
+
+
+// OUTPUT
+
+// Age of Vivek is 25
+// Age of Vignesh is 24
+// Age of Dinesh is 25
+// Age of Rasanth is 24

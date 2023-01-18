26919e9101aca786e10c5efa5300bd3ff0ad6130
vivekdevaraj96 <121920875+vivekdevaraj96@users.noreply.github.com>
2023-01-18 07:53:24 -0800

    Add files via upload

---
+++ JSONForOf.js
@@ -0,0 +1,18 @@
+var BikeModels={
+    "Yamaha":["R15 V4", "MT-15", "FZS 25", "FZ-S FI", "FZ-FI"],
+    "Bajaj" :["Pulsar", "Dominar", "Avenger", "Platina", "CT"],
+    "TVS"   :["Apache RR 310", "TVS Apache", "TVS Raider", "TVS Radeon", "TVS Sport"],
+};
+
+
+for(var models of BikeModels.Yamaha){
+    console.log(models);
+}
+
+for(var models of BikeModels.Bajaj){
+    console.log(models);
+}
+
+for(var models of BikeModels.TVS){
+    console.log(models);
+}

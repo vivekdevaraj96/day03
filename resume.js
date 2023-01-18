4c969de122243b188babef049d321ee72a2e5e25
vivekdevaraj96 <121920875+vivekdevaraj96@users.noreply.github.com>
2023-01-18 00:07:04 -0800

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,26 @@
+var resume={
+    "Name"               :"Vivek D",
+    "E-mail"             :"vivekdeva96@gmail.com",
+    "Mobile"             :"8122816639",
+    "D.O.B"              :"23/12/1996",
+    "Academic Profile"   :{
+        "UG"    :{
+            "Institute"  : "United Institute of Technology",
+            "Location"   : "Coimbatore",
+            "CGPA"       : "6.05",
+            "Year"       : "2014-2018"
+        },
+        "HSLC"  :{
+            "Institute"  : "Seventh-Day Adventist Matric Higher Sec School",
+            "Location"   : "Coimbatore",
+            "Percentage" : "52",
+            "Year"       : "2013-2014"
+        },
+        "SSLC"  :{
+            "Institute"  : "Ramnagar Suburban Matric Higher Sec School",
+            "Location"   : "Coimbatore",
+            "Percentage" : "72",
+            "Year"       : "2011-2012"
+        },
+    }     
+}

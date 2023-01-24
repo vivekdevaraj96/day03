9737f2534d6f1dbe1cd6d3118f3564794b7d8cfc
vivekdevaraj96 <121920875+vivekdevaraj96@users.noreply.github.com>
2023-01-24 04:01:33 -0800

    Add files via upload

---
+++ JSONforEach.js
@@ -0,0 +1,21 @@
+var studentDetails=[  {
+    "name":"Vivek",
+    "age" :"25"
+},{
+    "name":"Vignesh",
+    "age" :"24"
+},{
+    "name":"Dinesh",
+    "age" :"25"
+},{
+    "name":"Rasanth",
+    "age" :"24"
+}]
+
+studentDetails.forEach((s) => console.log(s));
+
+
+// { name: 'Vivek', age: '25' }
+// { name: 'Vignesh', age: '24' }
+// { name: 'Dinesh', age: '25' }
+// { name: 'Rasanth', age: '24' }
